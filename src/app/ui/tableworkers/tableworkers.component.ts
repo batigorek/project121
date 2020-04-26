d936c3d7e306113881d0e5b983c1892dfdc6a270
Милена Крюкова <batigorek@gmail.com>
2020-04-26 17:13:56 +0300

    'third'

--- src/app/ui/tableworkers/tableworkers.component.ts
+++ src/app/ui/tableworkers/tableworkers.component.ts
@@ -12,7 +12,13 @@ export class TableworkersComponent implements OnInit {
   @Input() workers: MyWorker[] = [];
   @Output() deleteWorker =
     new EventEmitter<number>();
-  type = 0;
+  @Output() changeWorker =
+    new EventEmitter<string>();
+
+  name: string;
+  surname: string;
+
+  flag: boolean = true;
   myWType = MyWorkerType;
   constructor() { }
 
@@ -21,5 +27,35 @@ export class TableworkersComponent implements OnInit {
 
   onDeleteWorker(id: number) {
     this.deleteWorker.emit(id);
+  }
+
+  onChangeName() {
+    switch (this.flag) {
+      case true:
+        this.flag = false;
+        break;
+      case false:
+        this.flag = true;
+        break;
+    }
+  }
+
+  onConfirmName(id: number) {
+    console.log(this.name);
+    this.workers[this.workers.findIndex((worker) => worker.id === id)].name = this.name;
+    this.workers[this.workers.findIndex((worker) => worker.id === id)].surname = this.surname;
+    console.log(this.name);
+    this.changeWorker.emit(this.surname);
+    if (((this.name != '')&& (this.name != undefined)) &&((this.surname != '')&& (this.surname != undefined)))  {
+      switch (this.flag) {
+        case true:
+          this.flag = false;
+          break;
+        case false:
+          this.flag = true;
+          break;
+      }
+    } 
+     
   }
 }
